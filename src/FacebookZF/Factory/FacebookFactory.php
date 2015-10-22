4473e6eb7e439cb6f8e6426edb06ca491bd21dee
Vinícius Nascimento Silva <vinicius.nascimento.silva@usp.br>
2015-10-22 13:50:46 -0200

    Initial Commit

---
+++ src/FacebookZF/Factory/FacebookFactory.php
@@ -0,0 +1,16 @@
+<?php
+namespace FacebookZF\Factory;
+
+use Zend\ServiceManager\FactoryInterface;
+
+class FacebookFactory implements FactoryInterface
+{
+
+    /* (non-PHPdoc)
+     * @see \Zend\ServiceManager\FactoryInterface::createService()
+     */
+    public function createService(ServiceLocatorInterface $services)
+    {
+        return null;
+    }
+}
